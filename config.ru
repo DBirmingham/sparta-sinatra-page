25cd27dbcb3a021c3a2cd777891c4c05a84f0b42
Tech-A03 <tech-a03@macbook-air-9.home>
2018-03-12 22:55:35 +0000

    I really need to get into the rhythm of committing

--- config.ru
+++ config.ru
@@ -1,10 +1,10 @@
 require 'sinatra'
 require 'pg'
 require 'sinatra/reloader' if development?
-require_relative './controllers/posts_controller.rb'
-require_relative './models/post.rb'
+require_relative './controllers/order_controller.rb'
+require_relative './models/order.rb'
 
 use Rack::Reloader
 use Rack::MethodOverride
 
-run PostsController
+run OrderController
--- controllers/order_controller.rb
+++ controllers/order_controller.rb
@@ -11,6 +11,60 @@ class OrderController < Sinatra::Base
   	end
 
   	get '/' do
-
-  		
+  		@title = "Order List"
+  		@orders = Order.all
+  		erb :'orders/index'
   	end
+
+  	get '/new' do
+  		@title = "New Burger"
+  		@order = Order.new
+    	@order.id = ""
+  		erb :'orders/new'
+  	end
+
+  	post '/' do
+    
+	    order = Order.new
+	    order.temp = params[:temp]
+	    order.size = params[:size]
+	    top = params[:topping].join(',')
+	    toppings = "{#{top}}"
+	    order.toppings = toppings
+
+	    order.save
+
+	    redirect '/'
+
+	end
+
+  	get '/:id' do
+  		id = params[:id].to_i
+  		@title = id
+  		@order = Order.grab id
+  		erb :'orders/show'
+  	end
+
+  	get '/:id/edit' do
+  		id = params[:id].to_i
+    	@order = Order.grab id
+    	erb :'orders/edit'
+	end
+
+	put '/:id'  do
+    
+	    id = params[:id].to_i
+	    top = params[:topping].join(',')
+		toppings = "{#{top}}"
+	    @order = Order.update(id, params[:temp], params[:size], toppings) 
+	    redirect '/'
+
+  	end
+
+  	delete '/:id' do
+  		id = params[:id].to_i
+    	Order.delete id
+    	redirect '/'
+    end
+
+end
